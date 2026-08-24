a583268470f8c189704d78826d5bc7593eb71177
Raygs Lima Monnerat <185018474+raygsm@users.noreply.github.com>
2026-08-24 06:55:53 -0300

    feat(clients): add Perola da Vila public route

---
+++ src/routes/peroladavila.tsx
@@ -0,0 +1,3 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { FullClientLanding } from "@/components/clients/FullClientLanding";
+export const Route = createFileRoute("/peroladavila")({ component: () => <FullClientLanding slug="peroladavila" /> });
